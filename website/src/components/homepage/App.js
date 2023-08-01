5e82806b6b07dbcaad2ceeca6d588e3e8e0003f2
bryannoelsalindeho <bryannoelsalindeho23@gmail.com>
2023-08-01 17:27:43 +0800

    organise image

--- website/src/components/homepage/App.js
+++ website/src/components/homepage/App.js
@@ -1,9 +1,24 @@
 import './App.css';
+import board from '../../images/board.png'
+import book from '../../images/book.png'
+import laptop from '../../images/laptop.png'
+import mug from '../../images/mug.png'
+import phone from '../../images/phone.png'
 
 function Homepage() {
   return (
     <div className="homepage">
-      <p>Home page</p>
+      <div className = "homepage__title">
+        <p className = "homepage__title__name">Bryan Noel Salindeho</p>
+        <p className = "homepage__title__role">SOFTWARE DEVELOPER | REACT, JAVA, SQL</p>
+      </div>
+      <div className = "homepage__image">
+        <img src = {board} alt = "board" className = "homepage__image__board"/>
+        <img src = {book} alt = "book" className = "homepage__image__book"/>
+        <img src = {laptop} alt = "laptop" className = "homepage__image__laptop"/>
+        <img src = {mug} alt = "mug" className = "homepage__image__mug"/>
+        <img src = {phone} alt = "phone" className = "homepage__image__phone"/>
+      </div>
     </div>
   );
 }
