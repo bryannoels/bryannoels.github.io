a6bb5279bc4dc811bdb6417b9389ae8a7b050d6b
bryannoelsalindeho <bryannoelsalindeho23@gmail.com>
2023-08-01 16:20:03 +0800

    remove react logo

--- website/src/App.js
+++ website/src/App.js
@@ -4,26 +4,11 @@ import WorkExperience from './components/workExperience/App';
 import Certificates from './components/certificates/App';
 import Education from './components/education/App';
 import Projects from './components/projects/App';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <Homepage/>
       <Profile/>
       <WorkExperience/>
