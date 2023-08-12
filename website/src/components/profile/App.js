c1796f79a860e1e69196bdd4c3c729161d7d51c0
bryannoelsalindeho <bryannoelsalindeho23@gmail.com>
2023-08-13 00:52:30 +0800

    profile page

--- website/src/components/profile/App.js
+++ website/src/components/profile/App.js
@@ -3,7 +3,20 @@ import './App.css';
 function Profile() {
   return (
     <div className="profile">
-      <p>Profile</p>
+      <div className="profile_left">
+        <div className="ipad_border">
+          <div className="ipad_inside">
+            <div className="grid-item photo">PHOTO</div>
+            <div className="grid-item about_me">ABOUT ME</div>
+            <div className="grid-item contact">CONTACT</div>
+            <div className="grid-item links">LINKS</div>
+            <div className="grid-item area_of_interest">AREA OF INTEREST</div>
+          </div>
+        </div>
+      </div>
+      <div className="profile_right">
+        <div className="mug"></div>
+      </div>
     </div>
   );
 }
