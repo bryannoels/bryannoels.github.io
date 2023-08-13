38c6feed209c7e3b094ec73894cee741505b9ac5
bryannoelsalindeho <bryannoelsalindeho23@gmail.com>
2023-08-13 13:55:49 +0800

    work experience

--- website/src/components/workExperience/App.js
+++ website/src/components/workExperience/App.js
@@ -3,7 +3,30 @@ import './App.css';
 function WorkExperience() {
   return (
     <div className="work-experience">
-      <p>Work Experience</p>
+      <div className="laptop">
+        <div className="laptop_inside">
+          <div className="browser_header">
+            <div className="browser_buttons">
+              <div className="button1" />
+              <div className="button2" />
+              <div className="button3" />
+              <div className="header_head" />
+            </div>
+            <div className="header_bottom">
+              <div className="browser_buttons2">
+                <div className="button1" />
+                <div className="button2" />
+                <div className="button3" />
+                <div className="search_bar2" />
+              </div>
+              <div className="search_bar" />
+            </div>
+          </div>
+          <div className="browser_body"></div>
+        </div>
+      </div>
+      <div className="laptop_middle"></div>
+      <div className="laptop_bottom"></div>
     </div>
   );
 }
