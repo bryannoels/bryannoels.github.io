0e45afc16cfcd7f210a23ee73e5cfd32b81c77d6
Bryan Noel Salindeho <bryannoelsalindeho23@gmail.com>
2023-08-25 04:00:37 +0800

    projects

--- website/src/components/projects/App.js
+++ website/src/components/projects/App.js
@@ -1,9 +1,52 @@
 import './App.css';
+import { useEffect } from 'react';
+import {projectsArray} from './constants.js';
 
 function Projects() {
+  let currentX = 0;
+
+  useEffect(() => {
+    const projectsTrack = document.querySelector(".projects_track");
+    const projectImages = document.querySelectorAll(".project_image");
+    const projectsTrackWidth = projectsTrack.getBoundingClientRect().width+1.2*window.innerWidth;
+
+    const handleWheel = (event) => {
+      if (currentX >= 0 && currentX <= projectsTrackWidth)
+        currentX = currentX + event.deltaX;
+      else if (currentX > projectsTrackWidth)
+        currentX = projectsTrackWidth;
+      else
+        currentX = 0;
+
+      const objectPositionX = (currentX / projectsTrackWidth) * 100;
+
+      projectImages.forEach((image) => {
+        image.animate({
+          objectPosition: `${objectPositionX}% center`
+        }, { duration: 1200, fill: "forwards" });
+      });
+
+      console.log("ScrollX:", currentX);
+    };
+
+    window.addEventListener("wheel", handleWheel);
+
+    return () => {
+      window.removeEventListener("wheel", handleWheel);
+    };
+  }, []);
   return (
     <div className="projects">
-      <p>Projects</p>
+      <div className="projects_track">
+        { projectsArray.map((project,id)=>(
+            <div className = "project_box" key = {project.id} style={{ backgroundColor: id % 3 === 0 ? "#B2D7DC" : id % 3 === 1 ? "#F3B03D" : "#F19D9B" }}>
+              <img className = "project_image" src = {project.image} alt={project.name}/>
+              <div className = "project_title">{project.title}</div>
+              <div className = "project_type">{project.type}</div>
+              <div className = "project_duration">{project.duration}</div>
+            </div>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ website/src/components/projects/constants.js
@@ -0,0 +1,101 @@
+import squirrel from '../../images/projects/squirrel.png';
+import lowdingShop from '../../images/projects/lowdingShop.png';
+
+export const projectsArray = [
+    {
+        id: "p1",
+        title: "SQUIRREL",
+        type: "Mobile Application",
+        duration: "MAR 2023 - MAY 2023",
+        description: `
+                • Designed an Android Mobile Application with a team of 2 members to help students study their modules.
+                • Applied Java and XML to create 14 mobile application screens (e.g. Login, Register, Module List, Topic List, Quiz) and React Typescript for the front-end of the dashboard (e.g. Student List, Student Results).
+                • Utilised SQL to build the database with 5 tables (i.e. students, results, modules, questions, and choices) and Java Servlet to form 14 Java Classes for the backend.`,
+        url: `https://github.com/bryannoels/SQUIRREL`,
+        image: squirrel,
+    },
+    {
+        id: "p2",
+        title: "SQUIRREL",
+        type: "Mobile Application",
+        duration: "MAR 2023 - MAY 2023",
+        description: `
+                • Designed an Android Mobile Application with a team of 2 members to help students study their modules.
+                • Applied Java and XML to create 14 mobile application screens (e.g. Login, Register, Module List, Topic List, Quiz) and React Typescript for the front-end of the dashboard (e.g. Student List, Student Results).
+                • Utilised SQL to build the database with 5 tables (i.e. students, results, modules, questions, and choices) and Java Servlet to form 14 Java Classes for the backend.`,
+        url: `https://github.com/bryannoels/SQUIRREL`,
+        image: lowdingShop,
+    },
+    {
+        id: "p3",
+        title: "SQUIRREL",
+        type: "Mobile Application",
+        duration: "MAR 2023 - MAY 2023",
+        description: `
+                • Designed an Android Mobile Application with a team of 2 members to help students study their modules.
+                • Applied Java and XML to create 14 mobile application screens (e.g. Login, Register, Module List, Topic List, Quiz) and React Typescript for the front-end of the dashboard (e.g. Student List, Student Results).
+                • Utilised SQL to build the database with 5 tables (i.e. students, results, modules, questions, and choices) and Java Servlet to form 14 Java Classes for the backend.`,
+        url: `https://github.com/bryannoels/SQUIRREL`,
+        image: lowdingShop,
+    },
+    {
+        id: "p4",
+        title: "SQUIRREL",
+        type: "Mobile Application",
+        duration: "MAR 2023 - MAY 2023",
+        description: `
+                • Designed an Android Mobile Application with a team of 2 members to help students study their modules.
+                • Applied Java and XML to create 14 mobile application screens (e.g. Login, Register, Module List, Topic List, Quiz) and React Typescript for the front-end of the dashboard (e.g. Student List, Student Results).
+                • Utilised SQL to build the database with 5 tables (i.e. students, results, modules, questions, and choices) and Java Servlet to form 14 Java Classes for the backend.`,
+        url: `https://github.com/bryannoels/SQUIRREL`,
+        image: lowdingShop,
+    },
+    {
+        id: "p5",
+        title: "SQUIRREL",
+        type: "Mobile Application",
+        duration: "MAR 2023 - MAY 2023",
+        description: `
+                • Designed an Android Mobile Application with a team of 2 members to help students study their modules.
+                • Applied Java and XML to create 14 mobile application screens (e.g. Login, Register, Module List, Topic List, Quiz) and React Typescript for the front-end of the dashboard (e.g. Student List, Student Results).
+                • Utilised SQL to build the database with 5 tables (i.e. students, results, modules, questions, and choices) and Java Servlet to form 14 Java Classes for the backend.`,
+        url: `https://github.com/bryannoels/SQUIRREL`,
+        image: lowdingShop,
+    },
+    {
+        id: "p6",
+        title: "SQUIRREL",
+        type: "Mobile Application",
+        duration: "MAR 2023 - MAY 2023",
+        description: `
+                • Designed an Android Mobile Application with a team of 2 members to help students study their modules.
+                • Applied Java and XML to create 14 mobile application screens (e.g. Login, Register, Module List, Topic List, Quiz) and React Typescript for the front-end of the dashboard (e.g. Student List, Student Results).
+                • Utilised SQL to build the database with 5 tables (i.e. students, results, modules, questions, and choices) and Java Servlet to form 14 Java Classes for the backend.`,
+        url: `https://github.com/bryannoels/SQUIRREL`,
+        image: lowdingShop,
+    },
+    {
+        id: "p7",
+        title: "SQUIRREL",
+        type: "Mobile Application",
+        duration: "MAR 2023 - MAY 2023",
+        description: `
+                • Designed an Android Mobile Application with a team of 2 members to help students study their modules.
+                • Applied Java and XML to create 14 mobile application screens (e.g. Login, Register, Module List, Topic List, Quiz) and React Typescript for the front-end of the dashboard (e.g. Student List, Student Results).
+                • Utilised SQL to build the database with 5 tables (i.e. students, results, modules, questions, and choices) and Java Servlet to form 14 Java Classes for the backend.`,
+        url: `https://github.com/bryannoels/SQUIRREL`,
+        image: lowdingShop,
+    },
+    {
+        id: "p8",
+        title: "SQUIRREL",
+        type: "Mobile Application",
+        duration: "MAR 2023 - MAY 2023",
+        description: `
+                • Designed an Android Mobile Application with a team of 2 members to help students study their modules.
+                • Applied Java and XML to create 14 mobile application screens (e.g. Login, Register, Module List, Topic List, Quiz) and React Typescript for the front-end of the dashboard (e.g. Student List, Student Results).
+                • Utilised SQL to build the database with 5 tables (i.e. students, results, modules, questions, and choices) and Java Servlet to form 14 Java Classes for the backend.`,
+        url: `https://github.com/bryannoels/SQUIRREL`,
+        image: lowdingShop,
+    },
+];
