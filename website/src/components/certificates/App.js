2a06407313bd86117b559c9407c6f06acf8439b1
Bryan Noel Salindeho <bryannoelsalindeho23@gmail.com>
2023-08-24 19:18:45 +0800

    certificates section

--- website/src/components/certificates/App.js
+++ website/src/components/certificates/App.js
@@ -1,9 +1,37 @@
 import './App.css';
+import {sortingOptions, certificatesArray} from './constants.js';
+
 
 function Certificates() {
   return (
     <div className="certificates">
-      <p>Certificates</p>
+      <div className="board">
+        <div className="board_head">
+          <div className="board_title">CERTIFICATES</div>
+          <div className="board_sort">
+            <div className="board_sort_title">SORT BY:</div>
+            <div className="board_sort_options">
+              {sortingOptions.map((option) => (
+                <div key={option} className="board_sort_option">{option}</div>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className="board_body">
+          {certificatesArray.map((certificate) => (
+            <div key={certificate.id} className="board_body_items">
+              <div className="board_body_certificate_image">
+                <img src={certificate.image} alt={certificate.name} />
+              </div>
+              <div className="board_body_certificate_description">
+                <div className="board_body_certificate_description_name">{certificate.name}</div>
+                <div className="board_body_certificate_description_issuer">Issued by {certificate.issuer}</div>
+                <div className="board_body_certificate_description_date">{certificate.date}</div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ website/src/components/certificates/constants.js
@@ -0,0 +1,68 @@
+import LinkedinLogo from '../../images/linkedin_logo.png';
+export const sortingOptions = ['DATE', 'NAME', 'ISSUER'];
+
+export const certificatesArray = [
+    {
+        id: "Li1",
+        image: LinkedinLogo,
+        name: "FROM REACT TO REACT NATIVE",
+        issuer: "LinkedIn",
+        date: "May 2023",
+    },
+    {
+        id: "Li1",
+        image: LinkedinLogo,
+        name: "FROM REACT TO REACT NATIVE",
+        issuer: "LinkedIn",
+        date: "May 2023",
+    },
+    {
+        id: "Li1",
+        image: LinkedinLogo,
+        name: "FROM REACT TO REACT NATIVE",
+        issuer: "LinkedIn",
+        date: "May 2023",
+    },
+    {
+        id: "Li1",
+        image: LinkedinLogo,
+        name: "FROM REACT TO REACT NATIVE",
+        issuer: "LinkedIn",
+        date: "May 2023",
+    },
+    {
+        id: "Li1",
+        image: LinkedinLogo,
+        name: "FROM REACT TO REACT NATIVE",
+        issuer: "LinkedIn",
+        date: "May 2023",
+    },
+    {
+        id: "Li1",
+        image: LinkedinLogo,
+        name: "FROM REACT TO REACT NATIVE",
+        issuer: "LinkedIn",
+        date: "May 2023",
+    },
+    {
+        id: "Li1",
+        image: LinkedinLogo,
+        name: "FROM REACT TO REACT NATIVE",
+        issuer: "LinkedIn",
+        date: "May 2023",
+    },
+    {
+        id: "Li1",
+        image: LinkedinLogo,
+        name: "FROM REACT TO REACT NATIVE",
+        issuer: "LinkedIn",
+        date: "May 2023",
+    },
+    {
+        id: "Li1",
+        image: LinkedinLogo,
+        name: "FROM REACT TO REACT NATIVE",
+        issuer: "LinkedIn",
+        date: "May 2023",
+    },
+];
